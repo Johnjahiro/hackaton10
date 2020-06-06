a0534b91da88750a60b2696aadf06e3763020e6e
jairo <jairo_cz01@hotmail.com>
2020-06-06 09:05:35 -0500

    Reto 1 terminado

---
+++ reto 1/app.js
@@ -0,0 +1,61 @@
+/* EJERCICIO 1 */
+
+// let suma = (a,b) => a+b
+
+// console.log("La suma es: ",suma(2,5))
+
+/* EJERCICIO 2 */
+
+// let pow, num
+
+// let potencia = (num,pow) => Math.pow(num,pow)
+
+// console.log("La potencia es: ", potencia(3,2))
+
+/* EJERCICIO 3 */
+
+// let a, b, c, suma
+
+// function sumaCubos(a,b,c) {
+//     suma = Math.pow(a,3) + Math.pow(b,3) + Math.pow(c,3)
+//     return suma
+// }
+
+// console.log("La suma de cubos es: ", sumaCubos(1,5,9))
+
+/* EJERCICIO 4 */
+
+// let areaTriangulo = (b,a) => (b*a)/2
+
+// console.log("El área del triángulo es: ", areaTriangulo(3,2))
+
+/* EJERCICIO 5 */
+
+// let n1, n2, operador;
+// let result;
+
+// function calculadora(n1, n2, operador) {
+//   switch (operador) {
+//     case '+':
+//       result = console.log("La suma es:",n1 + n2);
+//       break;
+//     case '-':
+//       result = console.log("La resta es:",n1 - n2);
+//       break;
+//     case '/':
+//       result = console.log("La división es:",n1 / n2);
+//       break;
+//     case 'x':
+//       result = console.log("El producto es:",n1 * n2);
+//       break;
+//     case '%':
+//       result = console.log("El módulo es:",n1 % n2);
+//       break;
+//     default:
+//         console.log("El parámetro no es reconocido")
+//       break;
+//   }
+//   return result
+// }
+
+// console.log(calculadora(3,2,'+'))
